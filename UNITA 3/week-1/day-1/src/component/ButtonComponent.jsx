8f3bf3457bae8679917dd3e2979be4aff0bc84fb
Riganelli Leonardo <leoriganelli1@gmail.com>
2024-11-30 14:54:58 +0100

    sistemazione finale

---
+++ UNITA 3/week-1/day-1/src/component/ButtonComponent.jsx
@@ -0,0 +1,11 @@
+const ButtonComponent = (props) => {
+    return (
+        <>
+            <button>
+                {props.text}
+            </button>
+        </>
+    )
+}
+
+export default ButtonComponent
---
+++ UNITA 3/week-1/day-1/src/component/ImageComponent.jsx
@@ -0,0 +1,9 @@
+const ImageComponent = () => {
+    return (
+        <>
+            <img src = "https://placedog.net/500/500" className="App-logo" alt="logo" />
+        </>
+    )
+}
+
+export default ImageComponent
