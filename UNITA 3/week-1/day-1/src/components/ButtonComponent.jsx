da54ce69986b56c6151f56bfb150eabcdfa7c33d
Riganelli Leonardo <leoriganelli1@gmail.com>
2024-11-30 14:32:55 +0100

    aggiornamento

--- UNITA 3/week-1/day-1/src/components/ButtonComponent.jsx
+++
@@ -1,8 +0,0 @@
-const ButtonComponent = (props) => {
-  // props is always going to be an object
-  // holding all the props you're passing to the components
-  // as keys
-  return <button>{props.label}</button>
-}
-
-export default ButtonComponent
