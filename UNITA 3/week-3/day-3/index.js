f2ddc44db00afa27da4483173d0790fa32dba85e
Riganelli Leonardo <leoriganelli1@gmail.com>
2024-12-11 16:45:57 +0100

    aggioranemnto

--- UNITA 3/week-3/day-3/index.js
+++ UNITA 3/week-3/day-3/index.js
@@ -1,75 +1,45 @@
-console.log('hello word')
-
+console.log('hello word');
 //1) Quali sono i tipi primitivi principali in TypeScript?
-
-console.log('i tipi primitivi principali di TypeScript sono: string, number, bollean, null e undefind.')
-
+console.log('i tipi primitivi principali di TypeScript sono: string, number, bollean, null e undefind.');
 //2) Crea tre variabili tipizzate: una stringa con il tuo nome, un numero con la tua età, e un booleano che indica se stai studiando TypeScript.
-
-const myName: string = 'Leonardo'
-const age: number = 22
-const studyTypeScript: boolean = true
-
-console.log('mi chiamo ' + myName + ',' + ' anni: '+ age + ',' + ' studio TypeScript: ' + studyTypeScript)
-
+var myName = 'Leonardo';
+var age = 22;
+var studyTypeScript = true;
+console.log('mi chiamo ' + myName + ',' + ' anni: ' + age + ',' + ' studio TypeScript: ' + studyTypeScript);
 //3) Tipizza il parametro della seguente funzione:
-
-const greet = (name: string) => { 
-
-    return "Ciao " + name
-}
-
-console.log(greet('leonardo'))
-
+var greet = function (name) {
+    return "Ciao " + name;
+};
+console.log(greet('leonardo'));
 //4) Specifica il tipo di ritorno della seguente funzione:
-
-const sum = (a: number, b: number) => {
-
-    return a + b 
-}
-
-console.log(sum(5, 10))
-
+var sum = function (a, b) {
+    return a + b;
+};
+console.log(sum(5, 10));
 //5) Crea una funzione che accetti un prezzo e restituisca il prezzo con IVA (22%). Usa i tipi appropriati.
-
-const price = (a: number) => { 
-    
-    let iva = 0
-    let sum = 0
-
-    iva = a * 22 / 100
-    sum = a + iva
-
-    return 'prezzo iniziale ' + a + ' totale ' + sum + ' di cui iva al 22%: ' + iva
-}
-
-console.log(price(500))
-
+var price = function (a) {
+    var iva = 0;
+    var sum = 0;
+    iva = a * 22 / 100;
+    sum = a + iva;
+    return 'prezzo iniziale ' + a + ' totale ' + sum + ' di cui iva al 22%: ' + iva;
+};
+console.log(price(500));
 //6) Crea una funzione che concateni due stringhe e restituisca la lunghezza totale.
-
-const parola = (str1: string, str2: string) => {
-
-    let stringa = ' '
-    stringa = str1.concat(str2)
-
-    return 'la parola ' + stringa + ' e lunga: ' + stringa.length
-}
-
-console.log(parola('fanta', 'stic'))
-
+var parola = function (str1, str2) {
+    var stringa = ' ';
+    stringa = str1.concat(str2);
+    return 'la parola ' + stringa + ' e lunga: ' + stringa.length;
+};
+console.log(parola('fanta', 'stic'));
 //7) Cos'è un Type Union e come si scrive?
-
-const variabile = (mix: string | number | undefined) => {
-    
-    console.log('la varibaile mix: ' + mix)
-}
-
-variabile(500)
-variabile('salve')
-variabile(undefined)
-
-console.log('come visto sopra la variabile e sempre la stessa, questo perche le Type Union sono variabili che possono essere piu tipi, ovvero un number o una stringa o un undefinde anziche soltanto uno di essi')
-
+var variabile = function (mix) {
+    console.log('la varibaile mix: ' + mix);
+};
+variabile(500);
+variabile('salve');
+variabile(undefined);
+console.log('come visto sopra la variabile e sempre la stessa, questo perche le Type Union sono variabili che possono essere piu tipi, ovvero un number o una stringa o un undefinde anziche soltanto uno di essi');
 /*8) Crea una variabile che possa contenere un numero, null o undefined.
 
 9) Crea un tipo per rappresentare i giorni della settimana usando union di stringhe letterali.
@@ -96,4 +66,4 @@ const numbers = [1, 2, 3]
 
 19) È possibile avere più tipi generici in un'interfaccia?
 
-20) Crea un'interfaccia generica per una risposta API.*/
+20) Crea un'interfaccia generica per una risposta API.*/ 
