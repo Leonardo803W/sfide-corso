2f2aee049670790bf9ccd0e41e097fb9b1bce35a
Riganelli Leonardo <leoriganelli1@gmail.com>
2025-01-07 17:04:12 +0100

    Create index.java

---
+++ CORSO/BACK AND/UNITA 1/week-1/day-1/index.java
@@ -0,0 +1,88 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //esercizio 1.1:
+
+        //System.out.println("inserrire il primo numero a scelta: ");
+        //int intero1 = scanner.nextInt();
+
+        int intero1 = 5;
+
+        //System.out.println("scegliere il secondo numero a scelta: ");
+        //int intero2 = scanner.nextInt();
+        int intero2 = 5;
+
+        int prodotto = 0;
+
+        System.out.println("il prodotto dei numeri e: " + intero1 * intero2);
+
+        //esercizio 1.2:
+
+        String world = "ciao";
+        int number = 6;
+
+        System.out.println(world + number);
+
+        //esercizio 1.3:
+
+        String[] resultArray = new String[6];
+
+        String[] inputArray = {"A", "B", "C", "D", "E"};
+        String word = "X";
+
+        resultArray[0] = inputArray[0];
+        resultArray[1] = inputArray[1];
+
+        resultArray[2] = word;
+
+        resultArray[3] = inputArray[2];
+        resultArray[4] = inputArray[3];
+        resultArray[5] = inputArray[4];
+
+        System.out.println(Arrays.toString(resultArray));
+
+        //esercizio 2.1:
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("prima frase: ");
+        String one = scanner.nextLine();
+
+        System.out.println("seconda frase: ");
+        String two = scanner.nextLine();
+
+        System.out.println("terza frase: ");
+        String three = scanner.nextLine();
+
+        System.out.println("concatenazione: " + one + two + three);
+        System.out.println("concatenzaione inversa: " + three + two + one);
+
+        //esercizio 3.1:
+
+        int n1 = 10;
+        double n2 = 12.5;
+        double perimeter = 0;
+        perimeter =+ n1 + n2 + n1 + n2;
+
+        System.out.println(perimeter);
+
+        //esercizio 3.2
+
+        int n3 = 4;
+
+        if(n3 % 2 == 0)
+        {
+            System.out.println(0);
+        }
+        else
+        {
+            System.out.println(1);
+        }
+
+        //esercizio 3.3:
+
+    }
+}
