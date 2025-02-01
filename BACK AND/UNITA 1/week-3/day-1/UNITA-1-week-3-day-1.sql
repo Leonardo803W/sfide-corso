6595980bb09e7379320f014bae0dc3c4ed0bd231
Riganelli Leonardo <leoriganelli1@gmail.com>
2025-02-01 16:30:14 +0100

    Update UNITA-1-week-3-day-1.sql

--- BACK AND/UNITA 1/week-3/day-1/UNITA-1-week-3-day-1.sql
+++ BACK AND/UNITA 1/week-3/day-1/UNITA-1-week-3-day-1.sql
@@ -31,14 +31,60 @@ CREATE TABLE Fatture (
 	IdFornitore INTEGER REFERENCES Fornitori(NumeroFornitore)
 );
 
-SELECT * FROM clienti WHERE nome = 'Mario';
-SELECT nome, cognome FROM clienti WHERE anno_di_nascita = 1982;
-SELECT COUNT(*) FROM fatture WHERE iva=20;
-SELECT * FROM prodotti WHERE (data_attivazione BETWEEN '2017-01-01' AND '2017-12-31') AND (in_produzione = true OR in_commercio = false);
-SELECT * FROM fatture WHERE importo < 1000 JOIN clienti ON id_cliente = numero_cliente;
-SELECT numero_fattura, importo, iva, denominazione data FROM fatture JOIN fornitori USING(numero_fornitore);
-SELECT COUNT(*), EXTRACT(YEAR FROM data_fattura) AS anno FROM fatture WHERE iva=20 GROUP BY anno;
-SELECT COUNT(*), SUM(importo), EXTRACT(YEAR FROM data_fattura) AS anno FROM fatture WHERE iva=20 GROUP BY anno;
-SELECT EXTRACT(YEAR FROM data_fattura), COUNT(*) AS totale FROM fatture WHERE tipologia = 'A' GROUP BY EXTRACT(YEAR FROM data_fattura) HAVING COUNT(*) >= 2;
-SELECT COUNT(*), SUM(importo), regione_residenza FROM fatture JOIN clienti ON id_cliente = numero_cliente GROUP BY regione_residenza;
-SELECT * FROM fatture WHERE id_cliente IN (SELECT numero_cliente FROM clienti WHERE anno_nascita = 1980) AND importo > 50;
+//inserimento di un solo elemento
+
+INSERT INTO clienti.clienti(Nome, Cognome, AnnoDiNascita, RegioneResidenza)
+					VALUES('marco', 'galbusera', 1945, 'lombardia')
+
+//inserimento di piu elementi in una volta sola
+
+INSERT INTO clienti(Nome, Cognome, AnnoDiNascita, RegioneResidenza)
+VALUES('marco', 'galbusera', '1945-03-21', 'lombardia'),
+							('luigi', 'nesi', '2005-05-21', 'veneto');
+
+//estrae solo gli elementi che contengono il valore 'lombardia'
+
+select * from clienti where regioneresidenza = 'lombardia';
+
+//estrae tutti gli elementi da tutti i campi senza condizione di valore
+
+select * from clienti;
+
+//estrae solo i campi nome contenenti il valore di condizione di where
+
+select nome from clienti where regioneresidenza = 'lombardia';
+
+//elimino la colonna cognome
+
+alter table clienti drop column cognome;
+
+//altero la tabella creando la colonna cognome;
+
+alter table clienti add cognome varchar(15) not null default ' ';
+
+//cambio il valore della colonna nome con condizione
+
+update clienti set nome = 'franco' where nome = 'marco';
+
+//elimino un valore nella colonna nome con condizione;
+
+delete from clienti where nome = 'leo';
+
+UPDATE fatture
+SET idcliente = clienti.numerocliente
+FROM clienti
+WHERE fatture.idcliente = clienti.numerocliente
+AND fatture.idcliente IS NULL;
+
+UPDATE fatture: Indica che stiamo aggiornando la tabella fatture.
+SET idcliente = clienti.id_cliente: Imposta il valore della colonna idcliente della tabella fatture al valore della colonna id_cliente della tabella clienti.
+FROM clienti: Specifica che stiamo unendo la tabella clienti.
+WHERE fatture.id_cliente = clienti.numero_cliente: Specifica la condizione di join tra le due tabelle.
+AND fatture.idcliente IS NULL: Limita laggiornamento solo alle righe in cui idcliente è NULL.
+
+esercizio: 
+
+1) select * from clienti where nome = 'mario';
+2) select * from clienti where annodinascita between '1982-01-01' and '1982-12-30';
+3) select * from fatture where iva = 20;
+4) select * from prodotti where dataattivazione  between '2017-01-01' and '2017-12-30';
